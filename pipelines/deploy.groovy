9ddd18d648589edf2a282101fbbf2c2c418b40b0
KarimZakzouk <karimzakzouk69@gmail.com>
2025-09-09 15:35:18 +0300

    Refactor Jenkins pipeline to use Docker Hub credentials for building and pushing images

--- pipelines/deploy.groovy
+++ pipelines/deploy.groovy
@@ -2,12 +2,21 @@ node {
     stage('Checkout'){
         checkout scm
     }
+
     stage('Build') {
-        echo 'Building...'
-        sh '''
-            docker build -t my-app:latest .
-        '''
+        withCredentials([
+            usernamePassword(
+                credentialsId: 'docker-hub-credentials',
+                usernameVariable: 'DOCKERHUB_USERNAME',
+                passwordVariable: 'DOCKERHUB_PASSWORD'
+            )
+        ]) {
+            sh '''
+                DOCKER_BUILDKIT=1 docker build -t $DOCKERHUB_USERNAME/my-app:latest .
+            '''
+        }
     }
+
     stage('Docker Login and Push') {
         withCredentials([
             usernamePassword(
@@ -18,9 +27,9 @@ node {
         ]) {
             sh '''
                 echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
-                docker push my-app:latest
+                docker push $DOCKERHUB_USERNAME/my-app:latest
+                docker rmi $DOCKERHUB_USERNAME/my-app:latest || true
             '''
         }
     }
-
 }
